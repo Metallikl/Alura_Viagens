b1279c8e7a58a6b4068209cd52bdbc74697c4b98
Daniel Luche <daniel.betteloni@gmail.com>
2020-01-12 13:16:35 -0300

    Inicio do curso de constraint layout. Finalizado aula 2. Proxima aula ,03, implementando o layout do pacote

---
+++ app/src/main/java/com/luche/aluraviagens/ui/activity/ListaPacotesActivity.kt
@@ -0,0 +1,13 @@
+package com.luche.aluraviagens.ui.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.luche.aluraviagens.R
+
+class ListaPacotesActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_lista_pacotes)
+    }
+}
